41a5802f3832613d747a6abc8a17395a2aca5e8f
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-09 13:20:01 -0500

    Retreive data from user input and display critera in console, append image to the page

--- script.js
+++ script.js
@@ -9,5 +9,22 @@ form.addEventListener("submit", async function (e) {
   const res = await axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${userSearch}&units=imperial&appid=b17d60e77dffd2e53cb818dad9614dfb`
   );
-  console.log(res.data);
+
+  //Create constants to store the JSON data
+  const weatherData = res.data;
+  // console.log(weatherData);
+  const cityName = weatherData.name;
+  console.log(cityName);
+  const temp = weatherData.main.temp;
+  console.log(`Temperature: ${temp}`);
+  const humidity = weatherData.main.humidity;
+  console.log(`Humidity: ${humidity}%`);
+  const windSpeed = weatherData.wind.speed;
+  console.log(`Wind Speed: ${windSpeed} MPH`);
+  const weatherIcon = weatherData.weather[0].icon;
+
+  const img = document.createElement("IMG");
+  img.src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+  document.body.append(img);
+  // weatherData.weather[0].icon;
 });
