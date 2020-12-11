b2ac42e894c3d9f6a9faab0d9cbb2cf8e2a46d9a
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-11 11:59:25 -0500

    return a promise for the uv index

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let form = document.querySelector("#searchForm");
-let queryDetails = document.querySelector("#queryDetails");
-let queryContent = document.querySelector("#queryContent");
-let cityname = document.querySelector("#cityname");
-let temp = document.querySelector("#temp");
-let humidity = document.querySelector("#humidity");
-let wind = document.querySelector("#wind");
+const form = document.querySelector("#searchForm");
+const queryDetails = document.querySelector("#queryDetails");
+const queryContent = document.querySelector("#queryContent");
+const cityname = document.querySelector("#cityname");
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
 
 // Declare Variables
-let searchHistory = [];
-let today = luxon.DateTime.local().toLocaleString();
+const searchHistory = [];
+const today = luxon.DateTime.local().toLocaleString();
 
 queryContent.hidden = true;
 // Create function to call api
-let currentWeather = async (userSearch) => {
-  let res = await axios.get(
+const currentWeather = async (userSearch) => {
+  const res = await axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${userSearch}&units=imperial&appid=b17d60e77dffd2e53cb818dad9614dfb`
   );
-  let weatherData = res.data;
+  const weatherData = res.data;
   // console.log(weatherData);
-  let weatherIcon = weatherData.weather[0].icon;
-  let img = document.createElement("IMG");
+  const weatherIcon = weatherData.weather[0].icon;
+  const img = document.createElement("IMG");
   img.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
 
   cityname.append(`${weatherData.name}  ${today}`);
@@ -27,6 +27,17 @@ let currentWeather = async (userSearch) => {
   temp.append(`Temperature: ${weatherData.main.temp} °F`);
   humidity.append(`Humidity: ${weatherData.main.humidity} %`);
   wind.append(`Wind Speed: ${weatherData.wind.speed} MPH`);
+  //UV Index
+  const lat = weatherData.coord.lat;
+  const lon = weatherData.coord.lon;
+
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b17d60e77dffd2e53cb818dad9614dfb`
+    )
+    .then(function (uviResponse) {
+      console.log(uviResponse);
+    });
 };
 
 //Add Event Listener on the input form
